f3818ed985163bc9c971bb90568be15952f2f727
Josh Bernstein <joshua.bernstein@outlook.com>
2019-09-28 12:52:10 -0600

    Added basic google map

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React from "react";
 
-import './App.css'
-import NavBar from './components/NavBar'
-import ResponderList from './components/ResponderList'
+import "./App.css";
+import NavBar from "./components/NavBar";
+import ResponderList from "./components/ResponderList";
+import GoogleMap from "./components/GoogleMap";
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
       <ResponderList />
+      <div>
+        <GoogleMap />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
