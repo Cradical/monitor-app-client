27564d43b8b2cb73817e67a7adca3af07a0ec956
Conrad Wright <easp.conrad@gmail.com>
2019-09-29 11:23:00 -0600

    Whole team view working with hard coded data

--- src/components/WholeTeamView/TeamMemberCard/index.js
+++ src/components/WholeTeamView/TeamMemberCard/index.js
@@ -13,86 +13,32 @@ import {
 import profileImage from '../../../assets/placeholder-profile_3.png'
 import './style.css'
 
-import { CrewContext } from '../../App'
+const STATUS = {
+  Good: 'success',
+  Warning: 'warning',
+  Danger: 'danger',
+}
 
 const TeamMemberCard = props => {
+  console.log(props.member.fname)
   return (
-    <div className='team-member-container'>
-      <CrewContext.Consumer>
-        {context => {
-          let uid = null
-          let fname = null
-          let lname = null
-          let hr = null
-          let bp_d = null
-          let bp_s = null
-          let lat = null
-          let lng = null
-          let air_supply = null
-          let env_temp = null
-          let internal_temp = null
-
-          let latest_hr = 100
-          let latest_bp_d = 120
-          let latest_bp_s = 90
-          let latest_lat = ''
-          let latest_lng = ''
-          let latest_air_supply = 15
-          let latest_env_temp = ''
-          let latest_internal_temp = 120
-
-          if (context) {
-            if (context[id]) {
-              uid = context[id].uid
-              fname = context[id].fname
-              lname = context[id].lname
-              hr = context[id].hr
-              bp_d = context[id].bp_d
-              bp_s = context[id].bp_s
-              lat = context[id].lat
-              lng = context[id].lng
-              air_supply = context[id].air_supply
-              env_temp = context[id].env_temp
-              internal_temp = context[id].internal_temp
-
-              latest_hr = hr[hr.length - 1].y
-              latest_bp_d = bp_d[bp_d.length - 1].y
-              latest_bp_s = bp_s[bp_s.length - 1].y
-              latest_lat = lat[lat.length - 1].y
-              latest_lng = lng[lng.length - 1].y
-              latest_air_supply = air_supply[air_supply.length - 1].y
-              latest_env_temp = env_temp[env_temp.length - 1].y
-              latest_internal_temp = internal_temp[internal_temp.length - 1].y
-            }
-          }
-
-          return (
-            <>
-              <div>
-                <Card className='card'>
-                  <CardImg
-                    top
-                    width='100%'
-                    src={profileImage}
-                    alt='Card image cap'
-                  />
-                  <CardBody className='card-body'>
-                    <CardTitle>{`${props.fname} ${props.lname}`}</CardTitle>
-                    <CardSubtitle>Firefighter</CardSubtitle>
-                    <CardText>
-                      <Badge className='status-pill' color='success' pill>
-                        <h6>Good</h6>
-                      </Badge>
-                    </CardText>
-                    <Button color='info'>Details</Button>
-                  </CardBody>
-                </Card>
-              </div>
-            </>
-          )
-        }}
-      </CrewContext.Consumer>
-    </div>
+    <Card className='card'>
+      <CardImg top width='100%' src={profileImage} alt='Card image cap' />
+      <CardBody className='card-body'>
+        <CardTitle>{`${props.member.fname} ${props.member.lname}`}</CardTitle>
+        <CardSubtitle>{props.member.title}</CardSubtitle>
+        <CardText>
+          <Badge
+            className='status-pill'
+            color={STATUS[props.member.status]}
+            pill
+          >
+            <h6>{props.member.status}</h6>
+          </Badge>
+        </CardText>
+        <Button color='info'>Details</Button>
+      </CardBody>
+    </Card>
   )
 }
 export default TeamMemberCard
--- src/components/WholeTeamView/index.js
+++ src/components/WholeTeamView/index.js
@@ -1,18 +1,55 @@
 import React from 'react'
 
 import TeamMemberCard from './TeamMemberCard'
+import './style.css'
 
 export default function WholeTeamView(props) {
-  console.log('props: ', props)
+  const data = [
+    {
+      fname: 'Jim',
+      lname: 'Tarence',
+      title: 'Firefighter',
+      team: 'Echo 6',
+      status: 'Good',
+    },
+    {
+      fname: 'Carey',
+      lname: 'User',
+      title: 'Tree Cutter',
+      team: 'Echo 6',
+      status: 'Good',
+    },
+    {
+      fname: 'Frank',
+      lname: 'Gilbert',
+      title: 'Ditch Digger',
+      team: 'Echo 6',
+      status: 'Good',
+    },
+    {
+      fname: 'Sarah',
+      lname: 'Fire',
+      title: 'Firefighter',
+      team: 'Echo 6',
+      status: 'Warning',
+    },
+    {
+      fname: 'John',
+      lname: 'Henry',
+      title: 'Track Layer',
+      team: 'Echo 6',
+      status: 'Danger',
+    },
+  ]
 
-  const teamMembers = props.map(teamMember => {
-    return <TeamMemberCard props={teamMember.user} />
+  const teamMembers = data.map(member => {
+    return <TeamMemberCard key={member.fname} member={member} />
   })
 
   return (
     <div>
       <h2>Team Overview</h2>
-      {teamMembers}
+      <div className='whole-team-view'>{teamMembers}</div>
     </div>
   )
 }
